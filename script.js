8f79cd85a9cd60dbf620bc4d7ad9420e74268b81
Danil <dancingdanil98@gmail.com>
2020-09-13 15:35:08 +0300

    Урок 15

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
-'use strict';
+"use strict";
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?",'');
 const personalMovieDB = {
-    count:0,
+    count: numberOfFilms,
     movies:{},
     actors:{},
     genres:[],
     privat:false
 };
-personalMovieDB.count = numberOfFilms;
-const movie = prompt('Один из последних просмотренных фильмов?');
-personalMovieDB.movies[movie] = prompt('На сколько оцените его?');
+
+
+for (let i = 0; i < 2; i++){
+    let movie = prompt('Один из последних просмотренных фильмов?');
+    let rate = prompt('Как вы оцените его');
+    if (movie != null && rate != null && movie != '' && rate != '' && movie.length < 50 && rate.length < 50 ){
+        personalMovieDB.movies[movie] = rate;
+    }
+    else{
+        i--;
+    }
+    
+}
+if (personalMovieDB.count < 10){
+    alert('Просмотрено довольно мало фильмов');
+}
+else if (10 <= personalMovieDB.count <= 30){
+    alert('Вы классический зритель');
+}
+else if (personalMovieDB.count > 30){
+    alert('Вы киноман');
+}
+else{
+    alert('Ошибка');
+}
 console.log(personalMovieDB);
 
 
