1c5d55a14aed2ee228673b297df3846d87134d38
Danil <dancingdanil98@gmail.com>
2020-09-14 09:45:36 +0300

    18 урок, практика

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 "use strict";
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?",'');
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?",'');
+while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+    numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?",'');
+}
+}
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies:{},
@@ -9,29 +18,51 @@ const personalMovieDB = {
 };
 
 
-for (let i = 0; i < 2; i++){
-    let movie = prompt('Один из последних просмотренных фильмов?');
-    let rate = prompt('Как вы оцените его');
-    if (movie != null && rate != null && movie != '' && rate != '' && movie.length < 50 && rate.length < 50 ){
-        personalMovieDB.movies[movie] = rate;
+function rememberMyFilms(){
+    for (let i = 0; i < 2; i++){
+        let movie = prompt('Один из последних просмотренных фильмов?');
+        let rate = prompt('Как вы оцените его');
+        if (movie != null && rate != null && movie != '' && rate != '' && movie.length < 50 && rate.length < 50 ){
+            personalMovieDB.movies[movie] = rate;
+        }
+        else{
+            i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10){
+        alert('Просмотрено довольно мало фильмов');
+    }
+    else if (10 <= personalMovieDB.count <= 30){
+        alert('Вы классический зритель');
+    }
+    else if (personalMovieDB.count > 30){
+        alert('Вы киноман');
     }
     else{
-        i--;
+        alert('Ошибка');
+    }    
+}
+detectPersonalLevel();
+
+function showMyDB(){
+    if (personalMovieDB.privat == false){
+        console.log(personalMovieDB);
     }
-    
 }
-if (personalMovieDB.count < 10){
-    alert('Просмотрено довольно мало фильмов');
+showMyDB();
+
+
+function writeYourGenres(){
+    for (let i = 1; i < 4; i++){
+        let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres.push(genre);
+    }
 }
-else if (10 <= personalMovieDB.count <= 30){
-    alert('Вы классический зритель');
-}
-else if (personalMovieDB.count > 30){
-    alert('Вы киноман');
-}
-else{
-    alert('Ошибка');
-}
-console.log(personalMovieDB);
+writeYourGenres();
 
 
